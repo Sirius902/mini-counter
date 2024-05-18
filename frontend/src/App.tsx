937135cfd0fee926d5cd94ca9a4fdf9248da5142
NSDawn <tnahsin.airus@gmail.com>
2024-05-17 19:33:05 -0700

    feat: added baseline front-end ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import miniLogo from './assets/mini-logo.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +7,23 @@ function App() {
 
   return (
     <>
+      <h1>Mini Counter</h1>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://www.miniusa.com/" target="_blank">
+          <img src={miniLogo} className="logo" alt="Mini logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+  
+      <h2>{count}</h2>
+      <div className="card buttons">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          +
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount(0)}>
+          c
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
